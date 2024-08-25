9022a273adb3a8427441756169c882b39f5f4893
yntianyu <elizabeth.rose.yan@gmail.com>
2024-08-25 00:03:32 -0700

    done

--- js/script.js
+++ js/script.js
@@ -40,8 +40,6 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
